c34a8042820d4faab4a1b89f45a654d79d5ef494
mpatsis <mpatsis13@gmail.com>
2019-03-21 16:44:42 +0000

    testing GenerateLMData.py

--- python/GenerateLMData.py
+++ python/GenerateLMData.py
@@ -12,13 +12,16 @@ from os.path import join
 
 def instance_tokens_generator(tokens_files):
     data_paths = list(map(lambda f: join(getcwd(), f), tokens_files))
+    if len(data_paths) is 0:
+        print("Must pass token*.json files and at least one data file")
+        sys.exit(1)
+    
     for token_seq in EncodedSequenceReader(data_paths):
         yield token_seq
         break
 
 def main(args):
-    tokens_files = args[1]
-    print(tokens_files)
+    tokens_files = args[1:]
     for token_seq in instance_tokens_generator(tokens_files):
         print(token_seq)
 
