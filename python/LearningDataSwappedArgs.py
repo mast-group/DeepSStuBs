0c40226d7cf8c6add4fbc481864835d8f57b52dd
mpatsis <mpatsis13@gmail.com>
2019-03-26 14:58:43 +0000

    Attempt to get function call text for querying ELMo

--- python/LearningDataSwappedArgs.py
+++ python/LearningDataSwappedArgs.py
@@ -71,6 +71,7 @@ class LearningData(object):
         callee_vector = name_to_vector[callee_string]
         argument0_vector = name_to_vector[argument_strings[0]]
         argument1_vector = name_to_vector[argument_strings[1]]
+        print(call["base"] + " " + callee_string + " " + argument_strings[0] + ", " + argument_strings[1])
         
         # optional information: base object, argument types, etc.
         base_string = call["base"]
@@ -120,6 +121,76 @@ class LearningData(object):
         if calls != None:
             calls.append(CodePiece(callee_string, argument_strings, call["src"]))
             
+    
+    def code_to_ELMo_xy_pairs(self, call, xs, ys, name_to_vector, type_to_vector, node_type_to_vector, calls=None):
+        arguments = call["arguments"]
+        self.stats["calls"] += 1
+        if len(arguments) != 2:
+            return
+        self.stats["calls_with_two_args"] += 1
+        
+        # mandatory information: callee and argument names        
+        callee_string = call["callee"]
+        argument_strings = call["arguments"]
+        if not (callee_string in name_to_vector):
+            return
+        for argument_string in argument_strings:
+            if not (argument_string in name_to_vector):
+                return
+        self.stats["calls_with_known_names"] += 1
+        callee_vector = name_to_vector[callee_string]
+        argument0_vector = name_to_vector[argument_strings[0]]
+        argument1_vector = name_to_vector[argument_strings[1]]
+        
+        # optional information: base object, argument types, etc.
+        base_string = call["base"]
+        base_vector = name_to_vector.get(base_string, [0]*name_embedding_size)
+        if base_string in name_to_vector:
+            self.stats["calls_with_known_base_object"] += 1
+        
+        argument_type_strings = call["argumentTypes"]
+        argument0_type_vector = type_to_vector.get(argument_type_strings[0], [0]*type_embedding_size)
+        argument1_type_vector = type_to_vector.get(argument_type_strings[1], [0]*type_embedding_size)
+        if (self.is_known_type(argument_type_strings[0]) or self.is_known_type(argument_type_strings[1])):
+            self.stats["calls_with_known_types"] += 1
+        if (self.is_known_type(argument_type_strings[0]) and self.is_known_type(argument_type_strings[1])):
+            self.stats["calls_with_both_known_types"] += 1
+        
+        parameter_strings = call["parameters"]
+        parameter0_vector = name_to_vector.get(parameter_strings[0], [0]*name_embedding_size)
+        parameter1_vector = name_to_vector.get(parameter_strings[1], [0]*name_embedding_size)
+        if (parameter_strings[0] in name_to_vector or parameter_strings[1] in name_to_vector):
+            self.stats["calls_with_known_parameters"] += 1
+        
+        # for all xy-pairs: y value = probability that incorrect
+        x_keep = callee_vector + argument0_vector + argument1_vector
+        x_keep += base_vector + argument0_type_vector + argument1_type_vector
+        x_keep += parameter0_vector + parameter1_vector #+ file_name_vector
+        y_keep = [0]
+        print("callee_vector: ", callee_vector)
+        print("argument0_vector: ", argument0_vector)
+        print("argument1_vector: ", argument1_vector)
+        print("base_vector: ", base_vector)
+        print("argument0_type_vector: ", argument0_type_vector)
+        print("argument1_type_vector: ", argument1_type_vector)
+        print("parameter0_vector: ", parameter0_vector)
+        print("parameter1_vector: ", parameter1_vector)
+        print("\n\n")
+        xs.append(x_keep)
+        ys.append(y_keep)
+        if calls != None:
+            calls.append(CodePiece(callee_string, argument_strings, call["src"]))
+        
+        x_swap = callee_vector + argument1_vector + argument0_vector
+        x_swap += base_vector + argument1_type_vector + argument0_type_vector
+        x_swap += parameter0_vector + parameter1_vector #+ file_name_vector
+        y_swap = [1]
+        xs.append(x_swap)
+        ys.append(y_swap)
+        if calls != None:
+            calls.append(CodePiece(callee_string, argument_strings, call["src"]))
+            
+    
     def anomaly_score(self, y_prediction_orig, y_prediction_changed):
         return y_prediction_orig - y_prediction_changed # higher means more likely to be anomaly in current code
     
