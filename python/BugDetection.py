efe988e00b83f595528e2986d54239b0127c91ea
mpatsis <mpatsis13@gmail.com>
2019-04-18 16:23:42 +0100

    Trying to debug.

--- python/BugDetection.py
+++ python/BugDetection.py
@@ -256,6 +256,7 @@ if __name__ == '__main__':
                     train_batches += 1
                     batch_loss, batch_accuracy = model.train_on_batch(batch_x, batch_y)
                     train_loss += batch_loss #* (batch_len / float(BATCH_SIZE))
+                    print('look', batch_accuracy, batch_len, float(BATCH_SIZE))
                     train_accuracy += batch_accuracy * (batch_len / float(BATCH_SIZE))
                     
                     if train_batches % 100 == 0: 
