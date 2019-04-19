85b633f0edcadce7b0bbb430f9df41763e427755
mpatsis <mpatsis13@gmail.com>
2019-04-19 16:10:14 +0100

    Removed debugging print statements.

--- python/BugDetection.py
+++ python/BugDetection.py
@@ -316,9 +316,7 @@ if __name__ == '__main__':
         continue
     try:
         while True:
-            print('Asked for batch')
             batch = batches_queue.get(timeout=5)
-            print('Hello batchness my old friend')
             batch_x, batch_y = batch
             batch_len = len(batch_x)
             test_instances += batch_len
