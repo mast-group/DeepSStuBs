f5defb46b1c2e5f3a221930e6f957b96353703a5
mpatsis <mpatsis13@gmail.com>
2019-05-10 10:30:40 +0100

    Debugging

--- python/LearningDataSwappedArgs.py
+++ python/LearningDataSwappedArgs.py
@@ -135,7 +135,9 @@ class LearningData(object):
         self.stats["calls_with_two_args"] += 1
         
         elmo_representations = ELMoModel.query([call["tokens"], call["swappedTokens"]])
+        print(elmo_representations)
         correct_vectors = elmo_representations[0][0]
+        print(correct_vectors)
         wrong_vectors = elmo_representations[0][1]
         
         x_keep = list(itertools.chain.from_iterable(correct_vectors))
