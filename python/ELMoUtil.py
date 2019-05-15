03563d271897760c551be8e48fb538473d203a1a
mpatsis <mpatsis13@gmail.com>
2019-05-15 14:17:47 +0100

    Testing centroid implementation.

--- python/ELMoUtil.py
+++ python/ELMoUtil.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
         print(context_ids)
 
         # Compute ELMo representations.
-        elmo_represenations_ = ELMoModel.query(context_ids, ElMoMode.CENTROID)
+        elmo_represenations_ = ELMoModel.query(context_ids, 2)
         # elmo_represenations_ = sess.run(
         #     elmo_token_op['weighted_op'],
         #     feed_dict={code_token_ids: context_ids}
