62ca5cf023b74d2c2c63b0a518613b3e1b53124b
mpatsis <mpatsis13@gmail.com>
2019-09-06 21:05:56 +0100

    Debugging

--- python/model/BPEModel.py
+++ python/model/BPEModel.py
@@ -397,8 +397,8 @@ class BPEModel(AbstractModel):
             
             bpe_token_representation = self._sess.run([self.model.next_state], feed_dict)
             print(bpe_token_representation)
-            print(bpe_token_representation[0])
-            print(bpe_token_representation[0].shape)
+            print(bpe_token_representation[0][0])
+            print(bpe_token_representation[0][0].shape)
 
             if True:
                 summed_representations = []
