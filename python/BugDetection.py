165e9896c5df5f5654eff7bfa63d0a66b221a363
mpatsis <mpatsis13@gmail.com>
2019-09-14 21:43:52 +0100

    Proper debug

--- python/BugDetection.py
+++ python/BugDetection.py
@@ -156,7 +156,6 @@ def minibatch_generator():
                 if len(code_pieces) > 0:
                     # Query the model for features
                     xs = learning_data.code_features(code_pieces, embeddings_model, emb_model_type, type_to_vector, node_type_to_vector)
-                    print(xs)
                     xs = tf.reshape(xs, [BATCH_SIZE, 600])
                     # for code_piece in code_pieces:
                     #     x = learning_data.code_features(code_piece, embeddings_model, emb_model_type, type_to_vector, node_type_to_vector)
@@ -174,6 +173,7 @@ def minibatch_generator():
             if len(code_pieces) == BATCH_SIZE:
                 # Query the model for features
                 xs = learning_data.code_features(code_pieces, embeddings_model, emb_model_type, type_to_vector, node_type_to_vector)
+                print(xs)
                 xs = tf.reshape(xs, [BATCH_SIZE, 600])
                 # for code_piece in code_pieces:
                 #     x = learning_data.code_features(code_piece, embeddings_model, emb_model_type, type_to_vector, node_type_to_vector)
