f5d69f8497e6175d1b6a46ebe7b2272c88c840db
mpatsis <mpatsis13@gmail.com>
2019-09-18 20:09:47 +0100

    Debug.

--- python/BugDetection.py
+++ python/BugDetection.py
@@ -588,6 +588,8 @@ if __name__ == '__main__':
                         batch_accuracy = batch_accuracy[1]
 
                         correct = 0.0
+                        for i in range(len(preds)):
+                            print(i)
                         for i, pred, label in zip(range(len(preds), preds, batch_y)):
                             if round(pred) == round(label):
                                 correct += 1
