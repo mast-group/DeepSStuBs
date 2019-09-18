81b308a70fc60bb044b6f06217c58cb19f3cd8bf
mpatsis <mpatsis13@gmail.com>
2019-09-18 20:35:33 +0100

    Debug.

--- python/BugDetection.py
+++ python/BugDetection.py
@@ -589,12 +589,10 @@ if __name__ == '__main__':
                         batch_accuracy = batch_accuracy[1]
 
                         correct = 0.0
-                        for i in range(len(preds.tolist())):
-                            print(i)
                         for i, pred, label in zip(range(len(preds.tolist())), preds.tolist(), batch_y.tolist()):
-                            if round(pred) == round(label):
+                            if round(pred[0]) == round(label[0]):
                                 correct += 1
-                        batch_accuracy = correct / len(pred)
+                        batch_accuracy = correct / len(preds.tolist())
                         
                         # batch_predictions = model.predict(batch_x)
                         # predictions.extend([pred for pred in batch_predictions])
