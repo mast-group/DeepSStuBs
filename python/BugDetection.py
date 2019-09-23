89b54634b59d00c8bf623fca0f795611d39b96c3
mpatsis <mpatsis13@gmail.com>
2019-09-23 01:17:41 +0100

    Batch size.

--- python/BugDetection.py
+++ python/BugDetection.py
@@ -62,7 +62,7 @@ EPOCHS = 10
 # Number of threads 
 BATCHING_THREADS = 1
 # Minibatch size. An even number is mandatory. A power of two is advised (for optimization purposes).
-BATCH_SIZE = 32#63 #256
+BATCH_SIZE = 100#63 #256
 # assert BATCH_SIZE % 2 == 0, "Batch size must be an even number."
 # Queue used to store code_pieces from_which minibatches are generated
 CODE_PIECES_QUEUE_SIZE = 1000000
