b15030d745cff07b62b9c2dd7811913d80ef9759
mpatsis <mpatsis13@gmail.com>
2019-09-23 20:47:57 +0100

    Bug fix.

--- python/BugDetection.py
+++ python/BugDetection.py
@@ -433,7 +433,7 @@ if __name__ == '__main__':
             with GRAPH.as_default():
                 # model = create_keras_network(dimensions)
                 ch_ids = embeddings_model.get_code_character_ids()
-                gather_op = tf.placeholder(shape=[BATCH_SIZE, 4, 2], dtype=tf.int32)
+                gather_op = tf.placeholder(shape=[BATCH_SIZE, int(dimensions / name_embedding_size), 2], dtype=tf.int32)
                 if what == "SwappedArgs":
                     e_op = embeddings_model.get_code_rep_op()['weighted_op']
                     inp_op = tf.gather_nd(e_op, gather_op)
