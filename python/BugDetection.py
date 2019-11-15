f3b216eadfd83a4f56b1368c88035128473dfc53
mpatsis <mpatsis13@gmail.com>
2019-11-15 11:02:20 +0000

    Undo.

--- python/BugDetection.py
+++ python/BugDetection.py
@@ -54,7 +54,7 @@ name_embedding_size = 200
 file_name_embedding_size = 50
 type_embedding_size = 5
 node_type_embedding_size = 8 # if changing here, then also change in LearningDataBinOperator
-extra_dims = 0
+
 Anomaly = namedtuple("Anomaly", ["message", "score"])
 
 # Number of training epochs
