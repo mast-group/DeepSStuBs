f9f5b0d39b0a7a9b35983374c5f380b7ee172872
mpatsis <mpatsis13@gmail.com>
2020-03-05 19:01:41 +0000

    Larger font

--- python/VisualizeContextualEmbeddings.py
+++ python/VisualizeContextualEmbeddings.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
         ax.set_ylabel('ylabel')
 
         for e, text in zip(mapped_example_embs, example_tokens[0]):
-            ax.text(e[0], e[1], text[text.index(':') + 1:], fontsize=4)
+            ax.text(e[0], e[1], text[text.index(':') + 1:], fontsize=6)
             print(e[0], e[1], text)
 
 
