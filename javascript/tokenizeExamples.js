5336a2df9fcf302b14b3528a2483e452a7d57f28
mpatsis <mpatsis13@gmail.com>
2020-03-05 11:46:26 +0000

    Parser argument.

--- javascript/tokenizeExamples.js
+++ javascript/tokenizeExamples.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const util = require("./jsExtractionUtil");
 
+var args = process.argv.slice(2);
 
+const path = args[0];
 const code = fs.readFileSync(path);
 const tokens = util.getTokens(code);
 if (tokens) {
